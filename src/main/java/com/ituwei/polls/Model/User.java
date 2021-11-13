bf74f6bab6c260dac5150d0e38114fe53571068b
Isack <ituwei21@gmail.com>
2021-11-14 00:45:03 +0300

    added model classes db

---
+++ src/main/java/com/ituwei/polls/Model/User.java
@@ -0,0 +1,112 @@
+package com.ituwei.polls.Model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.ituwei.polls.Model.audit.DateAudit;
+
+import org.hibernate.annotations.ManyToAny;
+import org.hibernate.annotations.NaturalId;
+
+@Entity
+@Table(name = "users", uniqueConstraints = { @UniqueConstraint(columnNames = { "username" }),
+        @UniqueConstraint(columnNames = { "email" }) })
+public class User extends DateAudit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotBlank
+    @Size(max = 15)
+    private String userName;
+    @NotBlank
+    @Size(max = 40)
+    private String name;
+
+    @NaturalId
+    @NotBlank
+    @Size(max = 40)
+    @Email
+    private String userEmail;
+    @NotBlank
+    @Size(max = 100)
+    private String userPassword;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "userId"), inverseJoinColumns = @JoinColumn(name = "roleId"))
+    private Set<Role> roles = new HashSet<>();
+
+    public User(String userName, String name, String userEmail, String userPassword) {
+        this.userName = userName;
+        this.name = name;
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+    }
+
+    public User() {
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+}
