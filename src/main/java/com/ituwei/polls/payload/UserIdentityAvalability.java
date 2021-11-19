acbbeebaf8f38eba6b5be80d090f266d7bacddd2
Isack <ituwei21@gmail.com>
2021-11-19 19:43:18 +0300

    added response Payload

---
+++ src/main/java/com/ituwei/polls/payload/UserIdentityAvalability.java
@@ -0,0 +1,17 @@
+package com.ituwei.polls.payload;
+
+public class UserAvailability{
+     private Boolean available;
+
+    public UserIdentityAvailability(Boolean available) {
+        this.available = available;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+}
